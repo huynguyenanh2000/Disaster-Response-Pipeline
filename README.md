202a00b9644937ec37de886f39b38b024a5a38f2
huynguyenanh24 <huynguyenanh24@gmail.com>
2022-11-17 16:31:36 +0700

    update project

--- README.md
+++ README.md
@@ -1,12 +1,68 @@
-# Disaster Response Pipeline Project
+# **Disaster Response Pipeline Project**
 
-### Instructions:
+
+## **Table of contents**
+
+- [Environment Setup](#environment-setup)
+- [Project Descriptions](#project-descriptions)
+- [File Structure](#file-structure)
+- [Usage](#usage)
+
+
+## **Environment Setup**
+
+**Environment**
+- OS: Windows 10
+
+- Interpreter: Visual Studio Code
+
+- Python version: Python 3.7
+
+**Libraries**
+- Install all packages using requirements.txt file using `pip install -r requirements.txt` command.
+
+**Link to GitHub Repository**
+
+`https://github.com/huynguyenanh2000/Disaster-Response-Pipeline.git`
+
+
+## **Project Descriptions**
+
+This project is a part of Udacity Data Scientist Nanodegree program. We apply text processing techniques(Tokenization, Lemmatization, Count Vectorizer, Tdidf, ...) and use Random Forest model to classify the messages of disaster event. 
+
+## **File Structure**
+
+~~~~~~~
+disaster_response_pipeline
+    |-- app
+        |-- templates
+                |-- go.html
+                |-- master.html
+        |-- run.py
+    |-- data
+        |-- DisasterResponseETL.db
+        |-- disaster_message.csv
+        |-- disaster_categories.csv
+        |-- process_data.py
+    |-- models
+        |-- DisasterResponseModel.pkl
+        |-- train_classifier.py
+    |-- Preparation
+        |-- ETL Pipeline Preparation.ipynb
+        |-- ML Pipeline Preparation.ipynb
+    |-- README
+    |-- requirements.txt
+~~~~~~~
+
+
+## **Usage**
+
 1. Run the following commands in the project's root directory to set up your database and model.
 
     - To run ETL pipeline that cleans data and stores in database
-        `python data/process_data.py data/disaster_messages.csv data/disaster_categories.csv data/DisasterResponse.db`
+        `python data/process_data.py data/disaster_messages.csv data/disaster_categories.csv data/DisasterResponseETL.db`
     - To run ML pipeline that trains classifier and saves
-        `python models/train_classifier.py data/DisasterResponse.db models/classifier.pkl`
+        `python models/train_classifier.py data/DisasterResponseETL.db models/DisasterResponseModel.pkl`
 
 2. Go to `app` directory: `cd app`
 
--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,8 +1,23 @@
 import sys
+import pandas as pd 
+import re
+from sqlalchemy import create_engine
+import nltk
+nltk.download(['stopwords','punkt','wordnet'])
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import classification_report
+import pickle
 
 
 def load_data(database_filepath):
-     """
+    """
     Load data from database
     
     Input: sqlite database file
@@ -11,36 +26,63 @@ def load_data(database_filepath):
     engine = create_engine('sqlite:///' + database_filepath)
     df = pd.read_sql_table(engine.table_names()[0], engine)
     
-    # Temporarily solution for noise data to apply binary classification
-    df = df[df["('related',)"] != 2]
-    
     X = df['message']
-    y = df[df.columns[4:]]
+    y = Y = df.drop(['id', 'message', 'original', 'genre'], axis = 1)
     
     return X, y
 
 
 def tokenize(text):
-    pass
+    text = re.sub('[^a-zA-Z0-9]',' ',text) # Remove characters that not in A-Z, a-z, 0-9
+    
+    text = text.lower() # Convert all characters to lowercase
+    
+    words = word_tokenize(text) # Tokenize text into list of words 
+    
+    enstopwords = stopwords.words("english")
+    words = [w for w in words if not w in enstopwords] # Remove stopwords in English
+    
+    lemmed = [WordNetLemmatizer().lemmatize(w) for w in words] # Lemmatize words
+    return lemmed
 
 
 def build_model():
-    pass
+    pipeline = Pipeline([
+                   ('vect', CountVectorizer(tokenizer=tokenize)),
+                   ('tfidf', TfidfTransformer()),
+                   ('clf', MultiOutputClassifier(RandomForestClassifier()))
+    ])
+
+    parameters = {
+        'clf__estimator__n_estimators': [1],
+    }
+
+    cv = GridSearchCV(pipeline, param_grid=parameters, verbose=2) 
+
+    return cv
 
 
-def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+
+def evaluate_model(model, X_test, Y_test):
+    pred = model.predict(X_test)
+
+    num_categories = Y_test.shape[1]
+    for index in range(num_categories):
+        col = Y_test.columns[index]
+        print(col)
+        print(classification_report(Y_test[col], pred[:, index]))
+
 
 
 def save_model(model, model_filepath):
-    pass
+    pickle.dump(model, open(model_filepath, 'wb'))
 
 
 def main():
     if len(sys.argv) == 3:
         database_filepath, model_filepath = sys.argv[1:]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
-        X, Y, category_names = load_data(database_filepath)
+        X, Y = load_data(database_filepath)
         X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
         
         print('Building model...')
@@ -50,7 +92,7 @@ def main():
         model.fit(X_train, Y_train)
         
         print('Evaluating model...')
-        evaluate_model(model, X_test, Y_test, category_names)
+        evaluate_model(model, X_test, Y_test)
 
         print('Saving model...\n    MODEL: {}'.format(model_filepath))
         save_model(model, model_filepath)
